56f6ed4e275f7039481cd4f37691bdeb7a0699e6
Morgan Stanley <morganpstanley@gmail.com>
2019-07-05 06:58:06 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  if block_given?
+    i = 0
+    while i < collection.length
+      yield array[i]
+      i += 1
+    end
+    array
+  else
+    puts "This Block should not run!"
+  end
+end
+
+my_each(i) do |i|
+  puts i
 end
