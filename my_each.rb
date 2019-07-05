b21dacfc1e4f418b2eca4bc2270fe044b762e6e8
Morgan Stanley <morganpstanley@gmail.com>
2019-07-05 07:16:48 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  if block_given?
+    i = 0
+    while i < array.length
+      yield array[i]
+      i = i + 1
+    end
+  else
+    return "This Block should not run!"
+  end
+  array
 end
+
+my_each([]) { |item| return item }
